1df76241283d5cdd70588e00550f67f207c8246d
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-05-20 11:32:17 +0300

    still learning

--- experiment2.c
+++ experiment2.c
@@ -22,8 +22,47 @@ int main(){
     printf("%s are blue. \n", noun);
     printf("I love %s", verb);
     */
-    int myArray[4];
-    printf("%d", myArray[2]);
+    //finding size of an array
+    /*
+    int i;
+    int sizeofArray;
+    int myArray[] = {43, 5, 34, 434, 67, 60};//6 entities
+    int length = sizeof(myArray);
+    printf("the size is : %d", length);
+    // length of an array =sizeof(arr)/sizeof(int)
+    sizeofArray = sizeof(myArray)/sizeof(int);
+    printf("the size of my array is : %d", sizeofArray);
+    */
+
+    //printing arrays
+    /*
+    int i, sizeofArray;
+    int myArray[] = {43, 5, 34, 434, 67, 60};//6 entities
+    sizeofArray = sizeof(myArray)/sizeof(int);
+    for(i=0; i<sizeofArray; i++){
+        printf("%d ", myArray[i]);
+    }*/
+    //2d array and nested loop
+    //int my2DArr[3][2] = {{45, 34}, {34, 80}, {444, 455}};
+    //printing multi dimensional array
+    /*
+    int i, j;
+    for(i=0; i<3; i++){
+        for(j=0; j<2; j++){
+            printf("%d ", my2DArr[i][j]);
+        }
+        printf("\n");
+    }
+    */
+    //pointers
+    /*
+    int x;
+    x =5;
+    int *ptr;
+    ptr = &x;//assigns address of x to pointer ptr
+    *ptr = 6;// changing value of object x
+    printf("%d\n", *ptr); //we can also say printf("%d\n", *ptr); which will give similar output
+    */
 
 
     return 0 ;
---
+++ fclose.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    /*
+    FILE *fp;
+
+    fp=fopen("fclosefile.txt", "w");
+    fprintf(fp, "%s", "this is an experiment on fopen and fclose\n");//fprintf(filepoiter, "%.." then massage
+    fprintf(fp, "%s", "am understanding better !!!!");
+    fclose(fp);
+    */
+
+    // C program to illustrate fgetc() function
